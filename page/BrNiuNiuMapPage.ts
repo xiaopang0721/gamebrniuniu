5937ad9032072ad5c2274be6e0f1307480b2a90c
happy <happy>
2019-09-15 21:32:52 +0800

    client批量提交

--- page/BrNiuNiuMapPage.ts
+++ page/BrNiuNiuMapPage.ts
@@ -130,6 +130,7 @@ module gamebrniuniu.page {
             }
             this._viewUI.mouseThrough = true;
             this._game.playMusic(Path_game_brniuniu.music_brniuniu + "nn_bgm.mp3");
+            this._viewUI.btn_spread.left = this._game.isFullScreen ? 25 : 10;
         }
 
         // 页面打开时执行函数
